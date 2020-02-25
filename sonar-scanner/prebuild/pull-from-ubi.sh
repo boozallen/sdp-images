d8cc659077ed8229bec87e13512e9a5aca65e0b2
CSpicer <spicer_casey@bah.com>
2020-02-25 16:48:01 -0500

    updated minor typos

--- sonar-scanner/prebuild/pull-from-ubi.sh
+++ sonar-scanner/prebuild/pull-from-ubi.sh
@@ -10,7 +10,7 @@ mkdir -p /root/prebuild/dependencies/sonar-scanner
 file=sonar-scanner-${SONAR_SCANNER_VERSION}-linux.zip
 curl --create-dirs -sSLo /root/prebuild/dependencies/sonar-scanner/${file} https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-linux.zip
 
-INSTALL_PKGS="yum-utils tar java-1.8.0-openjdk-devel openssl curl grep pgrep sed unzip which" && \
+INSTALL_PKGS="java-1.8.0-openjdk-devel curl grep pgrep sed unzip which" && \
     yum --nogpgcheck --disablerepo unified_platform_ubi8_appstream --disablerepo unified_platform_ubi8_os --disableplugin=subscription-manager -y update-minimal --setopt=tsflags=nodocs \
         --security --sec-severity=Important --sec-severity=Critical && \
     yum --nogpgcheck --disablerepo unified_platform_ubi8_appstream --disablerepo unified_platform_ubi8_os --disableplugin=subscription-manager -y install --setopt=tsflags=nodocs ${INSTALL_PKGS}
