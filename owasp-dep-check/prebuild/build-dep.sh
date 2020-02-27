3eb0f605b179412153769104d07a90e9a33ac89a
Karpagam Balan <balan_karpagam@bah.com>
2020-02-27 15:17:36 -0500

    Changes to add mono-devel in dependency bundle

--- owasp-dep-check/prebuild/build-dep.sh
+++ owasp-dep-check/prebuild/build-dep.sh
@@ -2,12 +2,12 @@
 
 set -xe
 
-SDP_DCAR_OWASP_DEP_CHK_VERSION=dcar-0.2
+SDP_DCAR_OWASP_DEP_CHK_VERSION=dcar-0.3
 
 cd /root/prebuild 
-tar czvf owasp-dep-check-$SDP_DCAR_OWASP_DEP_CHK_VERSION.tar.gz dependencies
+tar czvf owasp-dep-check-dependencies-$SDP_DCAR_OWASP_DEP_CHK_VERSION.tar.gz dependencies
 gpg --import /root/prebuild/BAH-public.key
 gpg --import --allow-secret-key-import  /root/prebuild/BAH-private.key
-gpg --output /root/prebuild/owasp-dep-check-$SDP_DCAR_OWASP_DEP_CHK_VERSION.sig --detach-sig /root/prebuild/owasp-dep-check-$SDP_DCAR_OWASP_DEP_CHK_VERSION.tar.gz
-sha256sum /root/prebuild/owasp-dep-check-$SDP_DCAR_OWASP_DEP_CHK_VERSION.tar.gz | awk '{print $1}' > /root/prebuild/owasp-dep-check-$SDP_DCAR_OWASP_DEP_CHK_VERSION.sha256
+gpg --output /root/prebuild/owasp-dep-check-dependencies-$SDP_DCAR_OWASP_DEP_CHK_VERSION.sig --detach-sig /root/prebuild/owasp-dep-check-dependencies-$SDP_DCAR_OWASP_DEP_CHK_VERSION.tar.gz
+sha256sum /root/prebuild/owasp-dep-check-dependencies-$SDP_DCAR_OWASP_DEP_CHK_VERSION.tar.gz | awk '{print $1}' > /root/prebuild/owasp-dep-check-dependencies-$SDP_DCAR_OWASP_DEP_CHK_VERSION.sha256
 rm -rf /root/prebuild/dependencies
--- owasp-dep-check/prebuild/pull-from-centos.sh
+++ owasp-dep-check/prebuild/pull-from-centos.sh
@@ -3,24 +3,37 @@
 set -xe
 
 OWASP_DEP_CHK_VERSION=5.2.4
-SDP_DCAR_OWASP_DEP_CHK_VERSION=dcar-0.2
+SDP_DCAR_OWASP_DEP_CHK_VERSION=dcar-0.3
 
 rm -rf /root/prebuild/dependencies
 
-mkdir -p /root/prebuild/dependencies/centos-release-scl /root/prebuild/dependencies/rh-ruby23 /root/prebuild/dependencies/rubygems-update /root/prebuild/dependencies/temp/rubygems-update /root/prebuild/dependencies/temp/bundle-audit /root/prebuild/dependencies/bundle-audit /root/prebuild/dependencies/owasp
+# Create relevant directories
 
-curl -sSLo /root/prebuild/dependencies/owasp/dependency-check-${SDP_DCAR_OWASP_DEP_CHK_VERSION}-release.zip https://dl.bintray.com/jeremy-long/owasp/dependency-check-${OWASP_DEP_CHK_VERSION}-release.zip 
+mkdir -p /root/prebuild/dependencies/mono-devel /root/prebuild/dependencies/centos-release-scl /root/prebuild/dependencies/rh-ruby23 /root/prebuild/dependencies/rubygems-update /root/prebuild/dependencies/temp/rubygems-update /root/prebuild/dependencies/temp/bundle-audit /root/prebuild/dependencies/bundle-audit /root/prebuild/dependencies/owasp
 
+# fetch mono-devel
+echo -e "[centos] \nname=CentOS-7\nbaseurl=http://mirror.vcu.edu/pub/gnu_linux/centos/7/os/x86_64/\nenabled=1\ngpgcheck=1\ngpgkey=http://mirror.vcu.edu/pub/gnu_linux/centos/7/os/x86_64/RPM-GPG-KEY-CentOS-7" > /etc/yum.repos.d/centos.repo
+
+su -c 'curl https://download.mono-project.com/repo/centos7-stable.repo | tee /etc/yum.repos.d/mono-centos7-stable.repo'
+
+
+yumdownloader mono-devel -y --resolve --destdir /root/prebuild/dependencies/mono-devel --disableplugin=subscription-manager
+
+# fetch dependency-check upstream binary
+curl -sSLo /root/prebuild/dependencies/owasp/dependency-check-${OWASP_DEP_CHK_VERSION}-release.zip https://dl.bintray.com/jeremy-long/owasp/dependency-check-${OWASP_DEP_CHK_VERSION}-release.zip 
+
+
+# fetch ruby dependencies
 yumdownloader centos-release-scl --resolve --destdir /root/prebuild/dependencies/centos-release-scl
 rpm -ivh --replacepkgs --replacefiles /root/prebuild/dependencies/centos-release-scl/*.rpm
 yumdownloader rh-ruby23 --resolve --destdir /root/prebuild/dependencies/rh-ruby23
 rpm -ivh --replacepkgs --replacefiles /root/prebuild/dependencies/rh-ruby23/*.rpm
 printf '\
-gem install "rubygems-update:<3.0.0"  -i /root/prebuild/dependencies/temp/rubygems-update --no-rdoc --no-ri && \
+gem install "rubygems-update:<3.0.0"  -i /root/prebuild/dependencies/temp/rubygems-update --no-document --no-rdoc --no-ri && \
 cp /root/prebuild/dependencies/temp/rubygems-update/cache/* /root/prebuild/dependencies/rubygems-update && \
 gem install --force --local /root/prebuild/dependencies/rubygems-update/*.gem && \
 update_rubygems && \
-gem install bundle-audit  -i /root/prebuild/dependencies/temp/bundle-audit --no-rdoc --no-ri && \
+gem install bundle-audit  -i /root/prebuild/dependencies/temp/bundle-audit --no-document --no-rdoc --no-ri && \
 cp /root/prebuild/dependencies/temp/bundle-audit/cache/* /root/prebuild/dependencies/bundle-audit && \
 gem install --force --local /root/prebuild/dependencies/bundle-audit/*.gem && \
 rm -rf /root/prebuild/dependencies/temp' | scl enable rh-ruby23 bash 
