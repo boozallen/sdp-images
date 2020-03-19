c1068ef7988520277cdd17d05e52cb254d3ea42f
caseymspicer@gmail.com <spicer_casey@bah.com>
2020-03-19 11:39:45 -0400

    Revert "Updated container to use openjdk1.8 (UBI8) base image"
    This reverts commit 273c8658b28f1cc96b5766a169af60d2335d00ea.

--- sonar-scanner/prebuild/build-dep.sh
+++ sonar-scanner/prebuild/build-dep.sh
@@ -2,12 +2,12 @@
 
 set -xe
 
-SDP_BUILD_DEPENDENCY_VERSION=dcar-0.2
+BRANCH_VERSION=dcar-0.2
 
 cd /root/prebuild
-tar czvf sonar-scanner-dependencies-$SDP_BUILD_DEPENDENCY_VERSION.tar.gz dependencies
+tar czvf sonar-scanner-dependencies-$BRANCH_VERSION.tar.gz dependencies
 gpg --import /root/prebuild/BAH-public.key
 gpg --import --allow-secret-key-import  /root/prebuild/BAH-private.key
-gpg --output /root/prebuild/sonar-scanner-dependencies-$SDP_BUILD_DEPENDENCY_VERSION.sig --detach-sig /root/prebuild/sonar-scanner-dependencies-$SDP_BUILD_DEPENDENCY_VERSION.tar.gz
-sha256sum /root/prebuild/sonar-scanner-dependencies-$SDP_BUILD_DEPENDENCY_VERSION.tar.gz | awk '{print $1}' > /root/prebuild/sonar-scanner-dependencies-$SDP_BUILD_DEPENDENCY_VERSION.sha256
+gpg --output /root/prebuild/sonar-scanner-dependencies-$BRANCH_VERSION.sig --detach-sig /root/prebuild/sonar-scanner-dependencies-$BRANCH_VERSION.tar.gz
+sha256sum /root/prebuild/sonar-scanner-dependencies-$BRANCH_VERSION.tar.gz | awk '{print $1}' > /root/prebuild/sonar-scanner-dependencies-$BRANCH_VERSION.sha256
 rm -rf /root/prebuild/dependencies
--- sonar-scanner/prebuild/pull-from-ubi.sh
+++ sonar-scanner/prebuild/pull-from-ubi.sh
@@ -10,8 +10,7 @@ mkdir -p /root/prebuild/dependencies/sonar-scanner
 file=sonar-scanner-${SONAR_SCANNER_VERSION}-linux.zip
 curl --create-dirs --insecure -sSLo /root/prebuild/dependencies/sonar-scanner/${file} https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-linux.zip
 
-INSTALL_PKGS="java-1.8.0-openjdk-devel curl grep sed unzip which" && \
-    yum clean all && \
+INSTALL_PKGS="java-1.8.0-openjdk-devel curl grep pgrep sed unzip which" && \
     yum --nogpgcheck --disablerepo unified_platform_ubi8_appstream --disablerepo unified_platform_ubi8_os --disableplugin=subscription-manager -y update-minimal --setopt=tsflags=nodocs \
         --security --sec-severity=Important --sec-severity=Critical && \
     yum --nogpgcheck --disablerepo unified_platform_ubi8_appstream --disablerepo unified_platform_ubi8_os --disableplugin=subscription-manager -y install --setopt=tsflags=nodocs ${INSTALL_PKGS}
