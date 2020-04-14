12928e951061214419b2e17514e281d40958c876
Karpagam Balan <balan_karpagam@bah.com>
2020-04-14 10:21:38 -0400

    Jenkins Agent built on ubi7

--- jenkins-agent/prebuild/build-dep.sh
+++ jenkins-agent/prebuild/build-dep.sh
@@ -4,10 +4,11 @@ set -xe
 
 JENKINS_AGENT_VERSION=dcar-0.9
 
-cd /root/prebuild
+cd /root/prebuild 
 tar czvf jenkins-agent-dependencies-$JENKINS_AGENT_VERSION.tar.gz dependencies
 gpg --import /root/prebuild/BAH-public.key
 gpg --import --allow-secret-key-import  /root/prebuild/BAH-private.key
 gpg --output /root/prebuild/jenkins-agent-dependencies-$JENKINS_AGENT_VERSION.sig --detach-sig /root/prebuild/jenkins-agent-dependencies-$JENKINS_AGENT_VERSION.tar.gz
 sha256sum /root/prebuild/jenkins-agent-dependencies-$JENKINS_AGENT_VERSION.tar.gz | awk '{print $1}' > /root/prebuild/jenkins-agent-dependencies-$JENKINS_AGENT_VERSION.sha256
 rm -rf /root/prebuild/dependencies
+
--- jenkins-agent/prebuild/pull-from-ubi.sh
+++ jenkins-agent/prebuild/pull-from-ubi.sh
@@ -4,7 +4,7 @@ set -xe
 
 JENKINS_AGENT_VERSION=dcar-0.9
 JENKINS_SWARM_VERSION=3.19
-JNLP_SLAVE_VERSION=3.17
+JNLP_SLAVE_VERSION=4.3
 
 rm -rf /root/prebuild/dependencies
 mkdir -p /root/prebuild/dependencies/swarm-client /root/prebuild/dependencies/agent /root/prebuild/dependencies/docker-ce /root/prebuild/dependencies/docker-compose /root/prebuild/dependencies/supervisor /root/prebuild/dependencies/hostname
@@ -12,10 +12,12 @@ mkdir -p /root/prebuild/dependencies/swarm-client /root/prebuild/dependencies/ag
 curl --create-dirs -sSLo /root/prebuild/dependencies/swarm-client/swarm-client-${JENKINS_SWARM_VERSION}.jar http://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/${JENKINS_SWARM_VERSION}/swarm-client-${JENKINS_SWARM_VERSION}.jar
 curl --create-dirs -sSLo /root/prebuild/dependencies/agent/remoting-${JNLP_SLAVE_VERSION}.jar http://repo.jenkins-ci.org/public/org/jenkins-ci/main/remoting/${JNLP_SLAVE_VERSION}/remoting-${JNLP_SLAVE_VERSION}.jar
 
-INSTALL_PKGS="tar hostname device-mapper-persistent-data lvm2 java-1.8.0-openjdk git openssl scl-utils python3 python36 python36-devel" && yum clean all &&\
-    yum --disableplugin=subscription-manager -y update-minimal --setopt=tsflags=nodocs \
-        --security && \
-    yum --disableplugin=subscription-manager -y install --setopt=tsflags=nodocs ${INSTALL_PKGS}
+INSTALL_PKGS="yum-utils tar hostname device-mapper-persistent-data lvm2 java-1.8.0-openjdk git openssl python27-python-pip gcc python27-python-devel.x86_64" && \
+    yum -y update-minimal --setopt=tsflags=nodocs \
+        --security  && \
+    yum -y install --setopt=tsflags=nodocs ${INSTALL_PKGS}
 
-pip3 install supervisor -d /root/prebuild/dependencies/supervisor/
-pip3 install docker-compose -d /root/prebuild/dependencies/docker-compose/
+
+printf 'pip download docker-compose -d /root/prebuild/dependencies/docker-compose' | scl enable python27 -
+printf 'pip download supervisor -d /root/prebuild/dependencies/supervisor' | scl enable python27 -
+
