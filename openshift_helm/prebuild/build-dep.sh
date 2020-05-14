73f806bb5566c9d44eb35af4d086a338449b0aa1
CSpicer-BAH <spicer_casey@bah.com>
2020-05-14 14:37:54 -0400

    updated helm and kubectl to latest version

--- openshift_helm/prebuild/build-dep.sh
+++ openshift_helm/prebuild/build-dep.sh
@@ -3,8 +3,8 @@
 set -xe
 
 OC_VERSION=v3.11.0
-HELM_VERSION=v2.15.2
-KUBECTL_VERSION=v1.14.7
+HELM_VERSION=v3.2.1
+KUBECTL_VERSION=v1.18.0
 
 rm -rf /root/prebuild/dependencies
 mkdir -p /root/prebuild/dependencies
@@ -15,19 +15,19 @@ file2=linux-amd64
 file3=kubectl
 
 #oc ${file1}
-curl -kLo /root/prebuild/dependencies/${file1}.tar.gz https://github.com/openshift/origin/releases/download/${OC_VERSION}/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+curl -kLo /root/prebuild/dependencies/${file1}.tar.gz https://github.com/openshift/origin/releases/download/${OC_VERSION}/${file1}.tar.gz
 tar -zxf ${file1}.tar.gz
 rm -rf ${file1}.tar.gz
 
 #helm ${file2}
-curl -ko /root/prebuild/dependencies/${file2}.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+curl -ko /root/prebuild/dependencies/${file2}.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-${file2}.tar.gz
 tar -zxf ${file2}.tar.gz
 rm -rf ${file2}.tar.gz
 
 #kubectl ${file3}
 curl -ko /root/prebuild/dependencies/${file3} https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
 
-SDP_BUILD_DEPENDENCY_VERSION=dcar-0.8
+SDP_BUILD_DEPENDENCY_VERSION=dcar-1.1
 
 cd /root/prebuild
 tar czvf helm-dependencies-$SDP_BUILD_DEPENDENCY_VERSION.tar.gz dependencies
