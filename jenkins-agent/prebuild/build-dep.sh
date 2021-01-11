14bce6bf101a1d0041de06e4b0d5096b8af556c5
CSpicer-BAH <spicer_casey@bah.com>
2021-01-11 14:22:48 -0500

    updated tarball for jenkins-agent

--- jenkins-agent/prebuild/build-dep.sh
+++ jenkins-agent/prebuild/build-dep.sh
@@ -2,9 +2,11 @@
 
 set -xe
 
-JENKINS_AGENT_VERSION=dcar-1.6
+JENKINS_AGENT_VERSION=dcar-1.7
 
 cd /root/prebuild
+rm -f dependencies/docker-ce/coreutils-8.30-8.el8.x86_64.rpm
+rm -f dependencies/docker-ce/coreutils-common-8.30-8.el8.x86_64.rpm
 tar czvf jenkins-agent-dependencies-$JENKINS_AGENT_VERSION.tar.gz dependencies
 gpg --import /root/prebuild/BAH-public.key
 gpg --import --allow-secret-key-import  /root/prebuild/BAH-private.key
