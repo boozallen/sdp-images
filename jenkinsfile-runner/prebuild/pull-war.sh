27bddcafdca469109ca4c3e0162f876466cf51c6
root <root@ip-10-0-0-201.ec2.internal>
2021-08-31 18:42:59 +0000

    updating dependencies

--- jenkinsfile-runner/prebuild/pull-war.sh
+++ jenkinsfile-runner/prebuild/pull-war.sh
@@ -10,7 +10,7 @@ mkdir -p /root/prebuild/dependencies/tmp /root/prebuild/dependencies/tmp/jenkins
 cp /usr/share/jenkins/jenkins.war /root/prebuild/dependencies/tmp/jenkins.war
 unzip -q /root/prebuild/dependencies/tmp/jenkins.war -d /root/prebuild/dependencies/tmp/jenkins/
 rm -rf \
-  /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/{commons-beanutils-1.9.3.jar,commons-fileupload-1.3.1-jenkins-2.jar,handlebars-1.1.1-core-assets.jar,commons-jelly-tags-fmt-1.0.jar,xstream-1.4.15.jar,jna-4.5.2.jar} \
+  /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/{commons-beanutils-1.9.3.jar,commons-fileupload-1.3.1-jenkins-2.jar,handlebars-1.1.1-core-assets.jar,commons-jelly-tags-fmt-1.0.jar,xstream-1.4.15.jar,xstream-1.4.17.jar,jna-4.5.2.jar} \
   /root/prebuild/dependencies/app/jenkins/{scripts,jsbundles,css,images,help,WEB-INF/detached-plugins,winstone.jar,WEB-INF/jenkins-cli.jar} \
   /root/prebuild/dependencies/app/repo/commons-jelly/commons-jelly-tags-fmt \
   /root/prebuild/dependencies/app/repo/org/apache/maven/maven-model/3.5.4/maven-model-3.5.4.jar \
@@ -18,7 +18,7 @@ rm -rf \
 
 cp /root/prebuild/dependencies/tmp/jenkins/winstone.jar /root/prebuild/dependencies/app/jenkins/winstone.jar
 cp /root/prebuild/dependencies/app/repo/com/google/guava/guava/30.1.1-jre/guava-30.1.1-jre.jar /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/
-cp /root/prebuild/dependencies/app/repo/com/thoughtworks/xstream/xstream/1.4.17/xstream-1.4.17.jar /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/
+cp /root/prebuild/dependencies/app/repo/com/thoughtworks/xstream/xstream/1.4.18/xstream-1.4.18.jar /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/
 cp /root/prebuild/dependencies/app/repo/commons-fileupload/commons-fileupload/1.4/commons-fileupload-1.4.jar /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/
 cp /root/prebuild/dependencies/app/repo/commons-beanutils/commons-beanutils/1.9.4/commons-beanutils-1.9.4.jar /root/prebuild/dependencies/app/jenkins/WEB-INF/lib/
 
